ba1ca17f67f0aee4dc97fad50266eda95345bb79
stan <9857@holbertonstudents.com>
2025-01-14 16:15:02 +0100

    Correction de la task 0

--- factorial.py
+++ factorial.py
@@ -2,26 +2,12 @@
 import sys
 
 def factorial(n):
-    """Calcule le factoriel d'un entier positif."""
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Décrémenter n pour éviter une boucle infinie
+        n -= 1  # Décrémenter la valeur de n
     return result
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./factorial.py <number>")
-        sys.exit(1)
-
-    try:
-        number = int(sys.argv[1])
-        if number < 0:
-            print("Le nombre doit être un entier positif.")
-            sys.exit(1)
-    except ValueError:
-        print("L'argument doit être un entier valide.")
-        sys.exit(1)
-
-    f = factorial(number)
+    f = factorial(int(sys.argv[1]))
     print(f)
